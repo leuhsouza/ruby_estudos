899004b9aba6554c27c1c3b14ca4a73ae147e3b1
leuhsouza <leuhsouza@gmail.com>
2023-12-06 17:09:27 -0300

    aula4

---
+++ ruby_estudos/ruby_puro/aula4/each_array.rb
@@ -0,0 +1,9 @@
+names = ['Joaozinho', 'Manoel', 'Juca']
+
+name = 'Leonardo Souza'
+
+names.each do |name|
+  puts name + ' é o meu nome'
+end
+
+puts name
---
+++ ruby_estudos/ruby_puro/aula4/each_hash.rb
@@ -0,0 +1,7 @@
+aulas = {'Aula 1 '=>'liberada', 'Aula 2' => 'liberada', 'Aula 3 ' => 'liberada', 'Aula 4' => 'liberada', 'Aula 5' => 'Em breve'}
+
+
+
+aulas.each do |key,value|
+  puts "#{key} #{value}"
+ end
