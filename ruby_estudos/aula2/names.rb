92faa4d9b14e4dd0b927a31da56ae8595bb38d3f
leuhsouza <leuhsouza@gmail.com>
2023-12-06 11:59:10 -0300

    aula2 finalizada

---
+++ ruby_estudos/aula2/names.rb
@@ -0,0 +1,6 @@
+print 'Qual o seu nome? '
+name = gets.chomp
+print ' Qual a sua idade? '
+age = gets.chomp
+
+puts "Olá #{name} que legal você tem #{age} anos"
---
+++ ruby_estudos/aula2/types.rb
@@ -0,0 +1,21 @@
+int = 10
+puts int.class
+int = 10.01
+puts int.class
+boolean = true
+puts boolean.class
+boolean = false
+puts boolean.class
+string = "Hello World"
+puts string.class
+leonardo_array =['position_0', 'Position_1', 'position_2']
+puts leonardo_array.class
+puts leonardo_array[0]
+leonardo_symbol = :ruby_symbol
+puts leonardo_symbol.class
+souza_symbol = :ruby_symbol
+puts souza_symbol
+puts leonardo_symbol.object_id
+puts souza_symbol.object_id
+leonardo_hash = {course: 'Ruby', language: 'pt-Br', locale: 'onebitcode.com'}
+puts leonardo_hash[:course]
