ef134c2ee783c41aa7b4ab3fac446035076b9625
leuhsouza <leuhsouza@gmail.com>
2024-03-06 06:44:21 -0300

    feat: aula finalizada

--- poo-study/class_study.rb
+++ poo-study/class_study.rb
@@ -20,13 +20,34 @@ puts al.mostra_nome
 =end
 
 class Aluno
-  attr_accessor :nome, :idade
+  attr_reader :nome, :idade
+  @last_name;
+  def initialize(nome,idade)
+    @nome = nome
+    @idade = idade
+  end
+
+  def add_last_name (last_name)
+    @last_name = last_name
+  end
+  def leitura
+    puts "Nome: #{self.nome} #{@last_name}"
+    puts "Idade: #{@idade}"
+  end
+
 end
 
-al = Aluno.new
+al = Aluno.new("Leonardo", 29)
+al.add_last_name("Souza")
+# al.nome = "Leonardo"
+# al.idade = 29
 
-al.nome = "Leonardo"
-al.idade = 29
-
+#posso chamar dessa forma abaixo se estiver dentro do initialize
 puts al.nome
 puts al.idade
+
+#assim não funciona, não tem acesso, só se estivesse dentro do metodo reader
+#puts al.last_name
+
+#fiz o metodo leitura
+al.leitura
