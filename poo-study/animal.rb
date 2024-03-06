0f680f3226aa9754584c2b6f4aca78fe35b65abb
leuhsouza <leuhsouza@gmail.com>
2024-03-06 07:16:45 -0300

    feat: add polymorphism concept

--- poo-study/animal.rb
+++ poo-study/animal.rb
@@ -3,6 +3,10 @@ class Animal
   def initialize (nome)
     @nome = nome
   end
+
+  def comunicar
+    puts "Olá eu sou a classe mãe ou super classe como preferir"
+  end
 end
 
 a1 = Animal.new("Jacaré")
@@ -17,9 +21,39 @@ class Cachorro < Animal
     super(nome)
     @raça = raça
   end
+
+  #polimorfismo de sobscrita
+  def comunicar
+    puts "Au, Au, Au = Eu sou a classe filha Cachorro"
+  end
 end
 
+class Gato < Animal
+  attr_reader :raça
+
+  def initialize(nome,raça)
+    super (nome)
+    @raça = raça
+  end
+
+  def comunicar
+    puts "Miau, Miau, Miau = Eu sou a classe filha Gato"
+  end
+
+  # def initialize(nome,raça)
+  #   super (nome,raça)
+  # end
+end
 bob = Cachorro.new("Bob", "bulldog")
 
 puts bob.nome
 puts bob.raça
+bob.comunicar
+
+puts "-----"
+
+xaninho = Gato.new("Xaninho","Persa")
+
+puts xaninho.nome
+puts xaninho.raça
+xaninho.comunicar
