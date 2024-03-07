68db35326e2e086d02a61f022e53ce54817df5a4
leuhsouza <leuhsouza@gmail.com>
2024-03-07 12:52:51 -0300

    feat: add production rate per hour metod

--- Exercicios/exercism/assembly_lyne.rb
+++ Exercicios/exercism/assembly_lyne.rb
@@ -7,15 +7,18 @@ class AssemblyLine
     def production_rate_per_hour
         if @speed <= 4
             @speed * 221
-        elsif @speed <=> 5 <=> 8
+        elsif @speed >= 5 and @speed <= 8
             @speed * 0.90 * 221
+        elsif @speed == 9
+            @speed * 0.80 * 221
+        else @speed * 0.77 * 221
+        end
     end
 
-    def mostrar
-        puts @speed
-    end
+    
 
 
-    a = AssemlyLine.new(5)
-    a.production_rate_per_hour
-    a.mostrar
+end
+    a = AssemblyLine.new(5)
+    p = a.production_rate_per_hour
+   
