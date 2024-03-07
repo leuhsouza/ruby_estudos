5d7dec96c14eb41f60a5807ca3e38b9f1e11ed00
leuhsouza <leuhsouza@gmail.com>
2024-03-06 21:49:16 -0300

    wip: ultima aula em andamento curso

---
+++ poo-study/turma/pessoa.rb
@@ -0,0 +1,8 @@
+class Pessoa
+  attr_reader :nome :idade
+  def initialize(nome,idade)
+    @nome = nome
+    @idade = idade
+  end
+
+end
---
+++ poo-study/turma/professor.rb
@@ -0,0 +1,9 @@
+require_relative 'pessoa'
+
+class Professor < Pessoa
+  attr_reader :matricula
+
+  def initialize(nome,idade,disciplina)
+    super(n)
+  end
+end
