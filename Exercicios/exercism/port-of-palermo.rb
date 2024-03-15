3893d2d0981432eb0b7db04c37a65d1d34b40567
leuhsouza <leuhsouza@gmail.com>
2024-03-15 00:00:21 -0300

    port of palermo

---
+++ Exercicios/exercism/port-of-palermo.rb
@@ -0,0 +1,11 @@
+module Port
+  :PALE
+
+  def self.get_identifier(city)
+    raise 'Please implement the Port.get_identifier method'
+  end
+
+  def self.get_terminal(ship_identifier)
+    raise 'Please implement the Port.get_terminal method'
+  end
+end
