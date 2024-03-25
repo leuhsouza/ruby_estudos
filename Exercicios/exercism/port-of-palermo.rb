b7f1fa60af9b3143523381e318805561c66db11c
leuhsouza <leuhsouza@gmail.com>
2024-03-25 19:27:29 -0300

    feat: city indentifier

--- Exercicios/exercism/port-of-palermo.rb
+++ Exercicios/exercism/port-of-palermo.rb
@@ -2,7 +2,8 @@ module Port
   IDENTIFIER = :PALE
 
   def self.get_identifier(city)
-    raise 'Please implement the Port.get_identifier method'
+    cityname = city[0,4].upcase
+    cityname.to_sym
   end
 
   def self.get_terminal(ship_identifier)
